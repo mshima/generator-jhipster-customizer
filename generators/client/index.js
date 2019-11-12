7327af524be874b99bfa317be9fbf4c8ee598bea
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-11-12 18:11:06 -0200

    Make jhipster-environment a singleton

--- generators/client/index.js
+++ generators/client/index.js
@@ -3,7 +3,7 @@ const debug = require('debug')('customizer:client');
 
 const jhipsterEnv = require('../../lib/jhipster-environment');
 
-module.exports = class extends jhipsterEnv.generator('client', { localOnly: true }) {
+module.exports = class extends jhipsterEnv.generator('client', { localOnly: true, root: true }) {
     constructor(args, opts) {
         debug('Initializing client blueprint');
         super(args, opts);
