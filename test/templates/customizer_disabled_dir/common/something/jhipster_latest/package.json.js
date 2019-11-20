16d8bab3c90c118c9160b928fc2c99d3b78843a3
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-11-20 10:52:45 -0200

    Add dir condition test.

---
+++ test/templates/customizer_disabled_dir/common/something/jhipster_latest/package.json.js
@@ -0,0 +1,14 @@
+const file = context => 'package.json';
+
+const tmpls = [
+    {
+        type: 'replaceContent',
+        target: /("generator-jhipster": )"[\\w.]*"/,
+        tmpl: '$1"latest"'
+    }
+];
+
+module.exports = {
+    file,
+    tmpls
+};
