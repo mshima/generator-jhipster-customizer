dc1e0028cb130b1c6850aca46b0f29b5d49b0763
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-05-22 17:54:11 -0300

    Fix no-unused-vars

--- test/spring-service.spec.js
+++ test/spring-service.spec.js
@@ -3,8 +3,6 @@ const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-const generatorsPath = require('../lib/environment').generatorsPath;
-
 describe('Subgenerator spring-service of customizer JHipster blueprint', () => {
   describe('Sample test', () => {
     before(function () {
