4a11200209a516c74c824ec9c5a5f08da9bfce4f
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-05-25 10:04:23 -0300

    Fixes

--- test/templates/user-jpa-identity/server/User.java.js
+++ test/templates/user-jpa-identity/server/User.java.js
@@ -1,4 +1,4 @@
-const file = context => `${context.constants.SERVER_MAIN_SRC_DIR}${context.packageFolder}/domain/User.java`;
+const file = context => `${context.constants.SERVER_MAIN_SRC_DIR}${context.storage.packageFolder}/domain/User.java`;
 
 const tmpls = [
   {
